585629c75cbeae390d764e77442c45d55d73b163
ziyonghong <1131002466@qq.com>
2020-02-07 17:25:19 +0800

    增加后台自动更新天气的功能

---
+++ app/src/main/java/com/example/coolweather/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,14 @@
+package com.example.coolweather.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.coolweather.service.AutoUpdateService;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, AutoUpdateService.class);
+        context.startService(i);
+    }
+}
